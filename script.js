92090b5cae4daff613e470c58ae8dc39ff3f85cb
Rafael Silveira <rafaelsilveira2011@live.com>
2022-07-27 15:05:31 -0300

    ADD Animações Scrolls

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function typeWrite(elemento){
     
 }
 
-
-
 const titulo = document.querySelector('.desktopH1');
 typeWrite(titulo)
 
@@ -19,3 +17,33 @@ typeWrite(spanWeb)
 
 const spanCi = document.querySelector('#spanCi');
 typeWrite(spanCi)
+
+/* Animacão*/
+
+const target = document.querySelectorAll('[data-anime]');
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add("animate");
+        }else{
+            element.classList.remove("animate");
+        }  
+    })
+}
+
+function showMenu(){
+    const windowTop = window.pageYOffset
+        if((windowTop) > 0){
+            document.querySelector(".menu").classList.add("animateMenuShow")
+        }else{
+            document.querySelector(".menu").classList.remove("animateMenuShow")
+        }  
+}
+
+window.addEventListener('scroll', function(){
+    animeScroll();
+    showMenu();
+})
+
